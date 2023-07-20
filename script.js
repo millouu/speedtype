3661087a33d125527f31bbf4f0955660a72a7939
millouu <srishti.chaurasia123@gmail.com>
2023-07-20 12:58:29 +0530

    logic done

--- script.js
+++ script.js
@@ -2,6 +2,32 @@ const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
 const quoteDisplayElement = document.getElementById("quoteDisplay");
 const quoteInputElement = document.getElementById("quoteInput");
 
+const timerElement = document.getElementById("timer");
+
+quoteInputElement.addEventListener("input", () => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll("span");
+  const arrayValue = quoteInputElement.value.split("");
+
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index];
+    let correct = true;
+
+    if (character == null) {
+      characterSpan.classList.remove("correct");
+      characterSpan.classList.remove("incorrect");
+      correct = false;
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add("correct");
+      characterSpan.classList.remove("incorrect");
+    } else {
+      characterSpan.classList.remove("correct");
+      characterSpan.classList.add("incorrect");
+      correct = false;
+    }
+  });
+  if (correct) getNextQuote();
+});
+
 function getRandomQuote() {
   return fetch(RANDOM_QUOTE_API_URL)
     .then((response) => response.json())
@@ -14,13 +40,27 @@ async function getNextQuote() {
   quoteDisplayElement.innerHTML = "";
   quote.split("").forEach((character) => {
     const characterSpan = document.createElement("span");
-    characterSpan.classList.add("correct");
+    // characterSpan.classList.add("correct");
     characterSpan.innerText = character;
     quoteDisplayElement.appendChild(characterSpan);
   });
 
   quoteInputElement.value = null;
+  startTimer();
   console.log(quote);
+}
+
+let startTime;
+function startTimer() {
+  timerElement.innerText = 0;
+  startTime = new Date();
+  setInterval(() => {
+    timerElement.innerText = getTimerTime();
+  }, 1000);
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000);
 }
 
 getNextQuote();
